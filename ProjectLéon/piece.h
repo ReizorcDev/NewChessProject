be6c5be0f44a7ec102932bffdde5bf271f7e40e5
Leon Delcroix <leondecheux@gmail.com>
2018-10-25 14:44:59 +0200

    avancement pieces.h

--- ProjectLéon/piece.h
+++ ProjectLéon/piece.h
@@ -1,9 +1,12 @@
 #pragma once
+typedef enum { Roi, Renne, Fou, Tour, Cavalier, Pion }classe;
 class piece
 {
 public:
+	classe classe;
 	int Xpos;
 	int Ypos;
+	int HP = 1;
 	piece();
 	~piece();
 };
